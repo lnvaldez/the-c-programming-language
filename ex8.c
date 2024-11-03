cd5b4b98d2a91fbedec96588a252ee9c870f31fb
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-03 10:44:05 -0300

    feat: initial solution

---
+++ ex8.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* Write a program to count blanks (' '), tabs ('/t),
+and newlines (\n)
+*/
+
+int main() {
+
+    int b, t, nl;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            ++b;
+        } else if (c == '\t') {
+            ++t;
+        } else if (c == '\n') {
+            ++nl;
+        }
+    };
+
+    printf("Blanks: %d\nTabs: %d \nNewlines: %d\n", b, t, nl);
+
+    return 0;
+
+}
