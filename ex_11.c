aaea37076f2c65312572eed006a9df77fbf829d0
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-04 11:33:49 -0300

    Add answer for exercise 1-11

--- ex_11.c
+++ ex_11.c
@@ -1,6 +1,10 @@
 /* Exercise 1-11. How would you test the word count program? 
 What kinds of input are most likely to uncover bugs if there are any? */
 
+/* Answer: Use of double dashes for emphasis will be
+counted as a single word */
+
+
 #include <stdio.h>
 
 #define IN 1 /* inside a word */
