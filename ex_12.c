bfffb353141dc52940498a6c4dddcb1efa5db300
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-04 11:45:20 -0300

    Add exercise 12 solution
    Read the line, if the character is blank, go to a newline and
    output the next set of characters

---
+++ ex_12.c
@@ -0,0 +1,28 @@
+/* Exercise 1-12. Write a program that prints 
+its input one word per line. */
+
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main() {
+
+	int c, word, state;
+	
+	state = OUT;
+	c = word = 0;
+	
+	while ((c = getchar()) != EOF) {
+
+		if (c == ' ') {
+			printf("\n");
+		} else {
+			putchar(c);
+		}
+
+	}		
+
+
+	return 0;
+}
