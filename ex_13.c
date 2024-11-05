eba75747564938cd9424aa0fc08ab526ca3d0507
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-05 11:32:23 -0300

    Add exercise 13 solution

--- ex_13.c
+++ ex_13.c
@@ -1,3 +1,27 @@
 /* Exercise 1-13. Write a program to print a histogram 
 of the lengths of words in its input. It is easy to draw the 
 histogram with the bars horizontal; a vertical orientation is more challenging. */
+
+/* Horizontal orientation */
+
+#include <stdio.h>
+
+int main()
+{
+	int c;
+	
+	while((c = getchar()) != EOF )
+	{
+		if (c == ' ' || c == '\n' || c == '\t')
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf("*", c);
+		}
+		
+	}
+	
+	return 0;
+}
