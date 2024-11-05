0fcf0cf9bf4fed7e22af29c7346b375d1452274d
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-05 11:48:08 -0300

    Add vertical_histogram function

--- ex_13.c
+++ ex_13.c
@@ -2,9 +2,44 @@
 of the lengths of words in its input. It is easy to draw the 
 histogram with the bars horizontal; a vertical orientation is more challenging. */
 
-/* Horizontal orientation */
-
 #include <stdio.h>
+#include <stdlib.h>
+
+void vertical_histogram()
+{
+	int* ptr;
+	int c, size;
+	
+	size = 0;
+	
+	while((c = getchar()) != EOF)
+	{
+		if (c == ' ' || c == '\n' || c == '\t')
+		{
+			;
+		}
+		else
+		{
+			++size;
+		}
+	}
+	
+	ptr = (int*)malloc(size * sizeof(int));
+	
+	for (int j = 0; j < size; ++j)
+	{
+		ptr[j] = j + 1;
+	}
+	
+	printf("The elements of the array are: ");
+	for (int k = 0; k < size; ++k)
+	{
+		printf("%d, ", ptr[k]);
+	}
+	
+}
+
+/* Horizontal orientation */
 
 int main()
 {
@@ -23,5 +58,10 @@ int main()
 		
 	}
 	
+	vertical_histogram();
+	
 	return 0;
 }
+
+/* Vertical orientation */
+
